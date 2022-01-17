cc597a5dc5a57c7ae28af3d7530b0896ef2cb93a
RyanJRasmussen <87948132+RyanJRasmussen@users.noreply.github.com>
2022-01-17 16:39:07 -0600

    Adding all changes

--- index.js
+++ index.js
@@ -1,6 +1,67 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+// Function to add all new images
+
+function newImage(img, right, up){
+    let item = document.createElement('img')
+    item.src = img
+    item.style.position = 'fixed'
+    item.style.left = right + 'px'
+    item.style.bottom = up + 'px'
+    document.body.append(item)
+    return item
+}
+
+
+// Function to add background images/ repeated images with a loop, calls previous function repeatedly. Works horizontally, put in new loop for vertical addition
+
+function addBackgroundImg(img, height){
+    
+    for(let i = 0; i < 20; i++){
+        newImage(img, 100 * i , height)
+    }
+}
+
+
+//Adding grass
+for(let i = 0; i < 5; i++){
+    addBackgroundImg('assets/grass.png', i * 100)
+}
+
+//Adding sky
+for(let i = 5; i < 10; i++){
+    addBackgroundImg('assets/sky.png', i * 100)
+}
+
+// adding character and environment images
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png ', 500, 425)
+
+
+// function to add items that can be picked up by double clicking
+
+function newItem(img, right, up){
+    let item = document.createElement('img')
+    item.src = img
+    item.style.position = 'fixed'
+    item.style.left = right + 'px'
+    item.style.bottom = up + 'px'
+    document.body.append(item)
+
+    item.addEventListener('dblclick', function(){
+        item.remove();
+    })
+}
+
+// adding item images
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/sheild.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
+
+
+
+
